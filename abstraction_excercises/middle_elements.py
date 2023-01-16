dab60100d8b518efca047e24d3d07af117539515
badgersky <szymonek.ptasznik@gmail.com>
2023-01-16 11:16:48 +0100

    created range-like class

--- abstraction_excercises/middle_elements.py
+++ abstraction_excercises/middle_elements.py
@@ -2,3 +2,21 @@ def middle_elements(iterators):
     return [iterator[int(len(iterator) / 2)] for iterator in iterators if iterator]
 
 
+class SequenceOfNumbers:
+
+    def __init__(self, start, stop, step):
+        self.step = step
+        self.stop = stop
+        self.start = start
+
+    def __len__(self):
+        return (self.stop - self.start) // self.step
+
+    def __getitem__(self, item):
+        if item < 0:
+            raise IndexError(f'Wrong Index: {item}')
+        if self.start + self.step * item >= self.stop:
+            raise IndexError(f'Wrong index: {item}')
+        return self.start + self.step * item
+
+
