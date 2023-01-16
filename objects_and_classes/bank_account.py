99672728857b9f92399d1d1a814d0ba90ced0e95
badgersky <szymonek.ptasznik@gmail.com>
2023-01-16 07:55:05 +0100

    added show_info functionality, tested object

--- objects_and_classes/bank_account.py
+++ objects_and_classes/bank_account.py
@@ -15,6 +15,28 @@ class BankAccount:
 
     def withdraw_cash(self, amount):
         if amount > 0.0:
-            return min(self.cash - amount, amount)
+            withdrawn_cash = min(self.cash, amount)
+            self.cash -= withdrawn_cash
+            return withdrawn_cash
         else:
             raise ValueError(f'Invalid amount: {amount}')
+
+    def show_info(self):
+        print(f'Account number: {self.number}\nBalance: {self.cash}')
+
+
+n = 2183961239786128931233
+account1 = BankAccount(n)
+
+# account1.deposit_cash(100)
+# account1.show_info()
+# account1.deposit_cash(200)
+# account1.show_info()
+# # account1.deposit_cash(-100)
+# print(account1.withdraw_cash(100))
+# account1.show_info()
+# print(account1.withdraw_cash(1000))
+# account1.show_info()
+# # print(account1.withdraw_cash(-200))
+# # account2 = BankAccount('siema')
+# # account3 = BankAccount(1231231232123.12312312312312312)
