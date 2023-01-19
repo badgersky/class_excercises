e9acda4ea8ff2c0ecc923ff39f37349679da77b2
badgersky <szymonek.ptasznik@gmail.com>
2023-01-19 12:57:53 +0100

    increased functionality by creating inheriting class

--- classes_homework/calculator.py
+++ classes_homework/calculator.py
@@ -18,8 +18,6 @@ class Calculator:
 
     @staticmethod
     def mul(a, b):
-        print(type(a))
-        print(type(b))
         try:
             if type(a) in [int, float] and type(b) in [int, float]:
                 return a * b
@@ -38,5 +36,38 @@ class Calculator:
             return f'Invalid arguments'
 
 
-calc = Calculator()
-print(calc.mul(2, 'eh'))
+class LoggingCalculator(Calculator):
+
+    def __init__(self):
+        self.history = []
+
+    def add(self, a, b):
+        result = super().add(a, b)
+        self.history.append(f'{a} + {b} = {result}')
+        return result
+
+    def mul(self, a, b):
+        result = super().mul(a, b)
+        self.history.append(f'{a} * {b} = {result}')
+        return result
+
+    def sub(self, a, b):
+        result = super().sub(a, b)
+        self.history.append(f'{a} - {b} = {result}')
+        return result
+
+    def div(self, a, b):
+        result = super().div(a, b)
+        self.history.append(f'{a} / {b} = {result}')
+        return result
+
+
+calc = LoggingCalculator()
+print(calc.add(2, 3))
+print(calc.mul(3, 3))
+print(calc.sub(7, 4))
+print(calc.div(5, 2))
+print(calc.history)
+
+
+
