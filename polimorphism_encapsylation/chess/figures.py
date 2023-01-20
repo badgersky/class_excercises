01b5831752f179f5916d18f508441eddf87e3d76
badgersky <szymonek.ptasznik@gmail.com>
2023-01-20 13:52:55 +0100

    created knight class with moving functionality

--- polimorphism_encapsylation/chess/figures.py
+++ polimorphism_encapsylation/chess/figures.py
@@ -34,3 +34,35 @@ class Pawn:
             self.y = y
         if self.moved is False:
             self.moved = True
+
+
+class Knight:
+
+    def __init__(self, color, y, x):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def list_allowed_moves(self, board):
+        allowed_moves = [
+                (self.x - 2, self.y - 1),
+                (self.x - 1, self.y - 2),
+                (self.x - 2, self.y + 1),
+                (self.x - 1, self.y + 2),
+                (self.x + 1, self.y + 2),
+                (self.x + 2, self.y + 1),
+                (self.x + 2, self.y - 1),
+                (self.x + 1, self.y - 2)
+            ]
+        for move in allowed_moves:
+            if move[0] > 7 or move[1] > 7:
+                allowed_moves.remove(move)
+            if move[0] < 0 or move[1] < 0:
+                allowed_moves.remove(move)
+        return allowed_moves
+
+    def move(self, x, y):
+        if (x, y) in self.list_allowed_moves(cb):
+            self.x = x
+            self.y = y
+
