c533f9d29445fd9b9b7e13517b5e1a978775c835
badgersky <szymonek.ptasznik@gmail.com>
2023-01-20 12:05:38 +0100

    added setter and getter functionalities

--- polimorphism_encapsylation/chess/chessboard.py
+++ polimorphism_encapsylation/chess/chessboard.py
@@ -1,5 +1,16 @@
 class Chessboard:
 
-    def __init__(self, color):
-        self.color = color
+    def __init__(self, color='white'):
+        self._color = color
         self.board = [[None for _ in range(8)] for _ in range(8)]
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, c):
+        if c.lower() not in ['white', 'black']:
+            raise ValueError(f'Invalid color: {c}')
+        self._color = c
+        
