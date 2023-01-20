9188fa3bc5ebc84cc2159f5089a69db524be4e82
badgersky <szymonek.ptasznik@gmail.com>
2023-01-20 14:31:48 +0100

    added rook class with moving functionalities, repaired bugs

--- polimorphism_encapsylation/chess/figures.py
+++ polimorphism_encapsylation/chess/figures.py
@@ -23,7 +23,7 @@ class Pawn:
         elif self.color.lower() == 'black':
             if self.moved is False:
                 return [(self.x, self.y - 1), (self.x, self.y - 2)]
-            elif self.moved is True and self.y < 7:
+            elif self.moved is True and self.y > 0:
                 return [(self.x, self.y - 1)]
             else:
                 return []
@@ -38,7 +38,7 @@ class Pawn:
 
 class Knight:
 
-    def __init__(self, color, y, x):
+    def __init__(self, color, x, y):
         self.x = x
         self.y = y
         self.color = color
@@ -54,15 +54,44 @@ class Knight:
                 (self.x + 2, self.y - 1),
                 (self.x + 1, self.y - 2)
             ]
-        for move in allowed_moves:
-            if move[0] > 7 or move[1] > 7:
-                allowed_moves.remove(move)
-            if move[0] < 0 or move[1] < 0:
-                allowed_moves.remove(move)
-        return allowed_moves
+        copied_moves = allowed_moves.copy()
+        for m in allowed_moves:
+            if m[0] < 0 or m[0] > 7:
+                copied_moves.remove(m)
+            elif m[1] < 0 or m[1] > 7:
+                copied_moves.remove(m)
+        return copied_moves
 
     def move(self, x, y):
         if (x, y) in self.list_allowed_moves(cb):
             self.x = x
             self.y = y
 
+
+class Rook:
+
+    def __init__(self, color, x, y):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def list_allowed_moves(self, board):
+        values = list(range(8))
+        allowed_moves = []
+        for value in values:
+            allowed_moves.append((self.x + value, self.y))
+        for value in values:
+            allowed_moves.append((self.x, self.y + value))
+
+        copied_moves = allowed_moves.copy()
+        for m in allowed_moves:
+            if m[0] < 0 or m[0] > 7:
+                copied_moves.remove(m)
+            elif m[1] < 0 or m[1] > 7:
+                copied_moves.remove(m)
+        return copied_moves
+
+    def move(self, x, y):
+            if (x, y) in self.list_allowed_moves(cb):
+                self.x = x
+                self.y = y
