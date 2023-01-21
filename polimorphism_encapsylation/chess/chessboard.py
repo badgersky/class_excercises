55f7b342aba1178eb3b3051110bb16aa4bbd23f2
badgersky <szymonek.ptasznik@gmail.com>
2023-01-21 15:52:11 +0100

    added creating figures functionality

--- polimorphism_encapsylation/chess/chessboard.py
+++ polimorphism_encapsylation/chess/chessboard.py
@@ -1,3 +1,7 @@
+import figures as f
+from pprint import pprint
+
+
 class Chessboard:
 
     def __init__(self, color='white'):
@@ -13,4 +17,44 @@ class Chessboard:
         if c.lower() not in ['white', 'black']:
             raise ValueError(f'Invalid color: {c}')
         self._color = c
-        
+
+    @staticmethod
+    def _create_white_figures():
+        color = 'white'
+        white_pawns = [f.Pawn(color, i, 1) for i, _ in enumerate(range(8))]
+        king = f.King(color, 4, 0)
+        queen = f.Queen(color, 3, 0)
+        other_figures = [
+            f.Rook(color, 0, 0),
+            f.Knight(color, 1, 0),
+            f.Bishop(color, 2, 0),
+            king,
+            queen,
+            f.Bishop(color, 5, 0),
+            f.Knight(color, 6, 0),
+            f.Rook(color, 7, 0)
+        ]
+        return white_pawns, other_figures
+
+    @staticmethod
+    def _create_black_figures():
+        color = 'black'
+        black_pawns = [f.Pawn(color, i, 6) for i, _ in enumerate(range(8))]
+        king = f.King(color, 4, 7)
+        queen = f.Queen(color, 3, 7)
+        other_figures = [
+            f.Rook(color, 0, 7),
+            f.Knight(color, 1, 7),
+            f.Bishop(color, 2, 7),
+            king,
+            queen,
+            f.Bishop(color, 5, 7),
+            f.Knight(color, 6, 7),
+            f.Rook(color, 7, 7)
+        ]
+        return black_pawns, other_figures
+
+
+if __name__ == '__main__':
+    cb = Chessboard()
+    pprint(cb.board)
